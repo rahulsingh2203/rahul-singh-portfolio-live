8a4aa15393ef240b24244ec9fafb8665d3d92602
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-01-25 09:57:02 +0530

    navbar scroll on click activated!

--- Scripts/navbar-scroll.js
+++ Scripts/navbar-scroll.js
@@ -14,3 +14,26 @@ function scrollFunction() {
 const footerCopyright = document.getElementById("footer-copyright");
 let date = new Date().getFullYear();
 footerCopyright.innerHTML = "@RahulSinghMeraNaam 1999-" + date;
+
+
+
+function navHome() {
+    //alert("it is working!");
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function navAbout() {
+    //alert("it is working!");
+    document.body.scrollTop = 700;
+    document.documentElement.scrollTop = 700
+}
+function navProjects() {
+    //alert("it is working!");
+    if (window.innerWidth < 400) {
+        document.body.scrollTop = 1850;
+        document.documentElement.scrollTop = 1850;
+    } else {
+        document.body.scrollTop = 1400;
+        document.documentElement.scrollTop = 1400;
+    }
+}
